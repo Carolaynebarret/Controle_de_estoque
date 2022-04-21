caddd4d3324772636e8680af1e4bd5516dcdf92f
Claudivan Barreto da Silva <claudivan_silva@apple.com>
2022-04-21 09:48:53 -0300

    chore: remove handlebars

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
 const express = require('express');
 const app = express();
-const { engine } = require('express-handlebars');
 const bodyParser = require('body-parser');
 const { Products } = require('./models');
 const moment = require('moment');
 const path = require('path');
 
-app.engine(
-  'handlebars',
-  engine({
-    defaultLayout: 'main',
-    helpers: {
-      formatDate: (date) => {
-        return moment(date).format('DD/MM/YYYY');
-      },
-    },
-  })
-);
-
-app.set('view engine', 'handlebars');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-//public
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/public'));
-//Rotas
 
 app.get('/produtos', function (req, res) {
   Products.findAll({ order: [['id', 'ASC']] }).then(function (produtos) {
@@ -49,13 +31,10 @@ app.post('/add-produto', function (req, res) {
   })
     .then(function () {
       res.redirect('/produtos');
-      //res.send("Pagamento cadastro com sucesso!")
     })
     .catch(function (erro) {
       res.send('Erro: Produto não foi cadastrado com sucesso!' + erro);
     });
-
-  //res.send("Nome: " + req.body.nome + "<br>Valor: " + req.body.valor + "<br>")
 });
 
 app.get('/del-pagamento/:id', function (req, res) {
