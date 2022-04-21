d75e6dfdb666be3f92d85d9b1541a8ffe904aa89
Carolaynebarret <carolaynebarreto@alu.uern.br>
2022-04-21 18:17:11 -0300

    feat: add create product

--- controllers/product.js
+++ controllers/product.js
@@ -6,7 +6,29 @@ module.exports = {
       const products = await Products.findAll({ order: [['id', 'ASC']] });
       return res.status(200).json({ products });
     } catch (error) {
-      return res.status(500).json({ message: 'SERVER_ERROR' });
+      return res.status(500).json({ message: 'SERVER_ERROR', error });
+    }
+  },
+  async create(req, res) {
+    try {
+      const product = await Products.create({
+        name: req.body.name,
+        quantity: req.body.quantity,
+        description: req.body.description,
+        category: req.body.category,
+        price: req.body.price,
+        lot: req.body.lot,
+        id_vendor: req.body.id_vendor,
+        manufacturing_date: req.body.manufacturing_date,
+        due_date: req.body.due_date,
+        vendor_cnpj: req.body.vendor_cnpj,
+        createdAt: req.body.createdAt,
+        updatedAt: req.body.updatedAt,
+      });
+
+      return res.status(201).json({ product });
+    } catch (error) {
+      return res.status(500).json({ message: 'SERVER_ERROR', error });
     }
   },
 };
--- routes/product.js
+++ routes/product.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const productController = require('../controllers/product');
 
 router.get('/products', productController.index);
+router.post('/products', productController.create);
 
 module.exports = router;
