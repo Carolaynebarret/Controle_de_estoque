1086d9cc4ed38242fe0a5b6b8d5a2165714d5c28
Claudivan Barreto da Silva <claudivan_silva@apple.com>
2022-04-22 10:51:24 -0300

    feat: add router navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import 'antd/dist/antd.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Products from './pages/Products/Products';
 import Sales from './pages/Sales/Sales';
 
 function App() {
   return (
-    <div className="App">
-      <Products />
-      <Sales />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="vendas" element={<Sales />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
